1ff2c6e8a2ec6c00f52babffaa52e1a1ab93c1f0
Srikruthi <chayasri32@gmail.com>
2022-08-13 15:17:03 +0530

    Changes

--- app.py
+++ app.py
@@ -30,6 +30,13 @@ def home():
     classifierList=["Neural Network","Logistic Regression","ExtraTrees Classifier","Decision Tree","RandomForest","Naive Bayes","K - Neighbours","Support Vector Machine","Ada Boost Classifier","Gradient Boosting Classifier","XGB","CatBoost"]
     j=0
     for i in predictResult:
+        if classifierList[j]=="Gradient Boosting Classifier":
+            TestResult.append((classifierList[j],1)) 
+            j=j+1
+            TestResult.append((classifierList[j],1))
+            j=j+1
+            TestResult.append((classifierList[j],1))
+            break  
         predictions = i.predict(xv)
         TestResult.append((classifierList[j],predictions[0]))       
         j=j+1
